e73b4afe0dd313afb253c7eb28e3579423ccc373
shrish <59884160+MSHRISH@users.noreply.github.com>
2021-06-29 20:07:10 +0530

    Update audio_to_text.py

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,14 +1,11 @@
 import speech_recognition as sr
-import pyaudio
-
 r = sr.Recognizer()
+
 def startConvertion():
-    mic=sr.Microphone(device_index=1)
-    print("listening")
-    with mic as s:
-        print("Yes")
+    audio_file = sr.AudioFile('file_name.wav')#specify the file name
+    with audio_file as s:
         r.adjust_for_ambient_noise(s)
-        audio=r.listen(s,timeout=10)
+        audio = r.record(s)
         try:
             print(r.recognize_google(audio,language="hi-IN"))
         except sr.UnknownValueError:
