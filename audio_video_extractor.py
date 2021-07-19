22dfabc73b89d17a6a70a932660ee880136a728e
shrish <59884160+MSHRISH@users.noreply.github.com>
2021-07-19 09:05:52 +0530

    Update audio_video_extractor.py

--- audio_video_extractor.py
+++ audio_video_extractor.py
@@ -3,10 +3,10 @@ import os
 import CONVRT
 
 def extract(url):
-    I=os.listdir("C:/Users/Dhanalakshmi/Desktop/dubber/Input_video")
-    J=os.listdir("C:/Users/Dhanalakshmi/Desktop/dubber/Input_audio")
-    v="C:/Users/Dhanalakshmi/Desktop/dubber/Input_video/"+"input_video"+str(len(I))+".mp4"
-    a="C:/Users/Dhanalakshmi/Desktop/dubber/Input_audio/"+"input_audio"+str(len(J))+".mp3"
+    I=os.listdir("#Path where downloaded video should be saved")
+    J=os.listdir("#Path where extracted audio should be saved")
+    v="#Path where downloaded video should be saved"+"input_video"+str(len(I))+".mp4"
+    a="#Path where extracted audio should be saved"+"input_audio"+str(len(J))+".mp3"
     FOLDER=[v,a]
     video=pafy.new(url)
     audios=video.audiostreams
@@ -14,8 +14,9 @@ def extract(url):
     best_video=video.getbest()
     best_audio.download(filepath = FOLDER[1])
     best_video.download(filepath=FOLDER[0])
+    
     CONVRT.convrt(FOLDER[1])
-    Converted_path="C:/Users/Dhanalakshmi/Desktop/dubber/Input_converted_audio"
+    Converted_path="#Path where Mp3 to wav converted audio should be saved"
     converted_audio_path=Converted_path+"/"+"Input_convert"+str(len(I))+".wav"
     FOLDER[1]=converted_audio_path
     return FOLDER
